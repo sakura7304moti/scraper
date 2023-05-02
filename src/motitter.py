3cd857a2fe6e4e03af8dd8c803f06fbdbbd114fd
DESKTOP-41T63M9\sakur <sakura7304moti@gmail.com>
2023-05-02 10:09:08 +0900

    no message

--- src/motitter.py
+++ src/motitter.py
@@ -54,12 +54,7 @@ def scrape(
             # write the csv header
             writer.writerow(header)
         while until_local <= datetime.datetime.strptime(until, "%Y-%m-%d"):
-            if type(since) != str:
-                until_local = datetime.datetime.strptime(
-                    until_local, "%Y-%m-%d"
-                ) + datetime.timedelta(days=interval)
-            else:
-                until_local = until_local + datetime.timedelta(days=interval)
+            until_local = until_local + datetime.timedelta(days=interval)
             scroll = 0
             if type(since) != str:
                 since = datetime.datetime.strftime(since, "%Y-%m-%d")
